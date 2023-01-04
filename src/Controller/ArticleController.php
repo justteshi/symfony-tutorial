43cbfadfa998b51ef7fa05488793659d1fae971b
Teodor Hristov <justteshi@gmail.com>
2023-01-04 14:26:25 +0200

    Add/ 01/6

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,10 +2,11 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class ArticleController
+class ArticleController extends AbstractController
 {
     /**
      * @Route("/")
@@ -20,6 +21,14 @@ class ArticleController
      */
     public function show($slug)
     {
-        return new Response(sprintf('Future news aricle: %s', $slug));
+        $comments = [
+            'Loren text etexteasd nawnjqwd',
+            'LOrem Ipsum atext awd nqwajksjh he',
+            'Ipsume text teshahhj ejkq asfjr fksmsj'
+        ];
+        return $this->render('article/show.html.twig',[
+            'text' => ucwords(str_replace('-',' ', $slug)),
+            'comments' => $comments
+        ]);
     }
 }
