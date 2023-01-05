2b1cf5e57cd90d27354ef7837f11a74aadc3dbbd
Teodor Hristov <justteshi@gmail.com>
2023-01-05 14:20:25 +0200

    Add/ 02/12

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -13,6 +13,14 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class ArticleController extends AbstractController
 {
+
+    private $isDebug;
+
+    public function __construct(bool $isDebug)
+    {
+        $this->isDebug = $isDebug;
+    }
+
     /**
      * @Route("/", name="app_homepage")
      */
