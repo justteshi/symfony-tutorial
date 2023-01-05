352e73aab41eb8d4034927c6b4f3b3d6801e4d44
Teodor Hristov <justteshi@gmail.com>
2023-01-05 14:13:19 +0200

    Add/ 02/11

--- src/Service/MarkdownHelper.php
+++ src/Service/MarkdownHelper.php
@@ -11,21 +11,28 @@ class MarkdownHelper
     private $markdown;
     private $cache;
     private $logger;
+    private $isDebug;
 
-    public function __construct(MarkdownInterface $markdown, AdapterInterface $cache, LoggerInterface $markdownLogger )
+    public function __construct(MarkdownInterface $markdown, AdapterInterface $cache, LoggerInterface $markdownLogger, bool $isDebug )
     {
         $this->markdown = $markdown;
         $this->cache = $cache;
         $this->logger = $markdownLogger;
+        $this->isDebug = $isDebug;
     }
     public function parse(string $source): string
     {
-        if(stripos($source, 'lorem') !== false)
-        {
+        if(stripos($source, 'lorem') !== false) {
             $this->logger->info('Lorem contains here');
         }
 
+        if($this->isDebug) {
+            return $this->markdown->transform($source);
+        }
+
+        dump($this->cache);die;
         $item = $this->cache->getItem('markdown_'.md5($source));
+
         if (!$item->isHit()) {
             $item->set($this->markdown->transform($source));
             $this->cache->save($item);
