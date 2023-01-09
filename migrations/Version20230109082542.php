1e58652a4ba0d8f370cd9d1195712fa6cc99ed26
Teodor Hristov <justteshi@gmail.com>
2023-01-09 11:21:35 +0200

    Add/ 03/17

---
+++ migrations/Version20230109082542.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230109082542 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE article ADD COLUMN created_at DATETIME DEFAULT NULL');
+        $this->addSql('ALTER TABLE article ADD COLUMN updated_at DATETIME DEFAULT NULL');
+        $this->addSql("UPDATE article SET created_at = date('now'), updated_at = date('now')");
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TEMPORARY TABLE __temp__article AS SELECT id, title, slug, content, published_at, author, heart_count, image_filename FROM article');
+        $this->addSql('DROP TABLE article');
+        $this->addSql('CREATE TABLE article (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, title VARCHAR(255) NOT NULL, slug VARCHAR(100) NOT NULL, content CLOB DEFAULT NULL, published_at DATETIME DEFAULT NULL, author VARCHAR(255) NOT NULL, heart_count INTEGER NOT NULL, image_filename VARCHAR(255) DEFAULT NULL)');
+        $this->addSql('INSERT INTO article (id, title, slug, content, published_at, author, heart_count, image_filename) SELECT id, title, slug, content, published_at, author, heart_count, image_filename FROM __temp__article');
+        $this->addSql('DROP TABLE __temp__article');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_23A0E66989D9B62 ON article (slug)');
+    }
+}
---
+++ migrations/Version20230109092102.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230109092102 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TEMPORARY TABLE __temp__article AS SELECT id, title, slug, content, published_at, author, heart_count, image_filename, created_at, updated_at FROM article');
+        $this->addSql('DROP TABLE article');
+        $this->addSql('CREATE TABLE article (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, title VARCHAR(255) NOT NULL, slug VARCHAR(100) NOT NULL, content CLOB DEFAULT NULL, published_at DATETIME DEFAULT NULL, author VARCHAR(255) NOT NULL, heart_count INTEGER NOT NULL, image_filename VARCHAR(255) DEFAULT NULL, created_at DATETIME NOT NULL, updated_at DATETIME NOT NULL)');
+        $this->addSql('INSERT INTO article (id, title, slug, content, published_at, author, heart_count, image_filename, created_at, updated_at) SELECT id, title, slug, content, published_at, author, heart_count, image_filename, created_at, updated_at FROM __temp__article');
+        $this->addSql('DROP TABLE __temp__article');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_23A0E66989D9B62 ON article (slug)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TEMPORARY TABLE __temp__article AS SELECT id, title, slug, content, published_at, author, heart_count, image_filename, created_at, updated_at FROM article');
+        $this->addSql('DROP TABLE article');
+        $this->addSql('CREATE TABLE article (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, title VARCHAR(255) NOT NULL, slug VARCHAR(100) NOT NULL, content CLOB DEFAULT NULL, published_at DATETIME DEFAULT NULL, author VARCHAR(255) NOT NULL, heart_count INTEGER NOT NULL, image_filename VARCHAR(255) DEFAULT NULL, created_at DATETIME DEFAULT NULL, updated_at DATETIME DEFAULT NULL)');
+        $this->addSql('INSERT INTO article (id, title, slug, content, published_at, author, heart_count, image_filename, created_at, updated_at) SELECT id, title, slug, content, published_at, author, heart_count, image_filename, created_at, updated_at FROM __temp__article');
+        $this->addSql('DROP TABLE __temp__article');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_23A0E66989D9B62 ON article (slug)');
+    }
+}
