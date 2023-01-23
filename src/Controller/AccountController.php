68395c36c44daa12dd33aadfc44e8b89eff08a8f
Teodor Hristov <justteshi@gmail.com>
2023-01-23 13:35:57 +0200

    Add/ 06/18

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -10,13 +10,14 @@ use Symfony\Component\Routing\Annotation\Route;
 /**
  * @IsGranted("ROLE_USER")
  */
-class AccountController extends AbstractController
+class AccountController extends BaseController
 {
     /**
      * @Route("/account", name="app_account")
      */
     public function index(): Response
     {
+        $user = $this->getUser()->getEmail();
         return $this->render('account/index.html.twig', []);
     }
 }
