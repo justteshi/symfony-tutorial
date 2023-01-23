c3148b3ab1a95e9bd9640f7a55dea544864e5cfc
Teodor Hristov <justteshi@gmail.com>
2023-01-23 15:51:30 +0200

    Add/ 06/23 Serializer

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -20,4 +20,16 @@ class AccountController extends BaseController
         $user = $this->getUser()->getEmail();
         return $this->render('account/index.html.twig', []);
     }
+
+    /**
+     * @Route("/api/account", name="api_account")
+     */
+    public function accountApi()
+    {
+        $user = $this->getUser();
+
+        return $this->json($user, 200, [], [
+            'groups' => ['main']
+        ]);
+    }
 }
