5e46aab028fd99a32a7b6c75d8fdbe928cca42bc
Teodor Hristov <justteshi@gmail.com>
2023-01-23 18:36:04 +0200

    Add/ 06/33 Finished

--- src/Controller/ArticleAdminController.php
+++ src/Controller/ArticleAdminController.php
@@ -23,13 +23,11 @@ class ArticleAdminController extends AbstractController
 
     /**
      * @Route ("/admin/article/{id}/edit")
+     * @IsGranted("MANAGE", subject="article")
      */
     public function edit(Article $article)
     {
-        //$article->getAuthor() != $this->getUser() && !$this->isGranted('ROLE_ADMIN_ARTICLE')
-        if (!$this->isGranted('MANAGE', $article)) {
-            throw $this->createAccessDeniedException('No access !');
-        }
+//        $this->denyAccessUnlessGranted('MANAGE', $article);
         dd($article);
     }
 
