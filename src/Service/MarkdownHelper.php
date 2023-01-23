ab37a45814013df89a96aee124ddfaba3e939c7d
Teodor Hristov <justteshi@gmail.com>
2023-01-23 14:22:42 +0200

    Add/ 06/20

--- src/Service/MarkdownHelper.php
+++ src/Service/MarkdownHelper.php
@@ -5,6 +5,7 @@ namespace App\Service;
 use Michelf\MarkdownInterface;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\Cache\Adapter\AdapterInterface;
+use Symfony\Component\Security\Core\Security;
 
 class MarkdownHelper
 {
@@ -12,25 +13,32 @@ class MarkdownHelper
     private $cache;
     private $logger;
     private $isDebug;
+    /**
+     * @var Security
+     */
+    private $security;
 
-    public function __construct(MarkdownInterface $markdown, AdapterInterface $cache, LoggerInterface $markdownLogger, bool $isDebug )
+    public function __construct(MarkdownInterface $markdown, AdapterInterface $cache, LoggerInterface $markdownLogger, bool $isDebug, Security $security)
     {
         $this->markdown = $markdown;
         $this->cache = $cache;
         $this->logger = $markdownLogger;
         $this->isDebug = $isDebug;
+        $this->security = $security;
     }
     public function parse(string $source): string
     {
         if(stripos($source, 'lorem') !== false) {
-            $this->logger->info('Lorem contains here');
+            $this->logger->info('Lorem contains here',[
+                'user' => $this->security->getUser()
+            ]);
         }
 
         if($this->isDebug) {
             return $this->markdown->transform($source);
         }
 
-        dump($this->cache);die;
+
         $item = $this->cache->getItem('markdown_'.md5($source));
 
         if (!$item->isHit()) {
