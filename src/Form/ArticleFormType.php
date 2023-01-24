39c95f621f4e232377c250f66acda14fd5e9ac0d
Teodor Hristov <justteshi@gmail.com>
2023-01-24 16:45:37 +0200

    Add/ 07/4 Data class

--- src/Form/ArticleFormType.php
+++ src/Form/ArticleFormType.php
@@ -2,8 +2,10 @@
 
 namespace App\Form;
 
+use App\Entity\Article;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class ArticleFormType extends AbstractType
 {
@@ -14,5 +16,13 @@ class ArticleFormType extends AbstractType
             ->add('content')
         ;
     }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+           'data_class' => Article::class
+        ]);
+    }
+
 
 }
