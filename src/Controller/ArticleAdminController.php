b692ce45d451e33617a26415a7bbd9c9892dfd01
Teodor Hristov <justteshi@gmail.com>
2023-01-24 15:08:34 +0200

    Add/ 07/2 Form submit

--- src/Controller/ArticleAdminController.php
+++ src/Controller/ArticleAdminController.php
@@ -8,6 +8,7 @@ use App\Repository\ArticleRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -17,9 +18,22 @@ class ArticleAdminController extends AbstractController
      * @Route ("/admin/article/new", name="admin_article_new")
      * @IsGranted("ROLE_ADMIN_ARTICLE")
      */
-    public function new(EntityManagerInterface $em)
+    public function new(EntityManagerInterface $em, Request $request)
     {
         $form = $this->createForm(ArticleFormType::class);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+            $article = new Article();
+            $article
+                ->setTitle($data['title'])
+                ->setContent('content')
+                ->setAuthor($this->getUser())
+            ;
+            $em->persist($article);
+            $em->flush();
+            return $this->redirectToRoute('admin_article_list');
+        }
         return $this->render('article_admin/new.html.twig', [
             'articleForm' => $form->createView(),
         ]);
@@ -36,12 +50,12 @@ class ArticleAdminController extends AbstractController
     }
 
     /**
-     * @Route("/admin/article")
+     * @Route("/admin/article", name="admin_article_list")
      * @IsGranted("ROLE_ADMIN_ARTICLE")
      */
     public function allArticles(ArticleRepository $articleRepo) {
         $articles = $articleRepo->findall();
-        return $this->render('article_admin/index.html.twig', [
+        return $this->render('article_admin/list.html.twig', [
             'articles'=> $articles
         ]);
     }
--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -127,6 +127,11 @@ class Article
         return $this->publishedAt;
     }
 
+    public function isPublished(): bool
+    {
+        return$this->publishedAt !== null;
+    }
+
     public function setPublishedAt(?\DateTimeInterface $publishedAt): self
     {
         $this->publishedAt = $publishedAt;
