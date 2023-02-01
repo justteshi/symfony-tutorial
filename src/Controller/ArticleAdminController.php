6ed1ee76dec152ff896e44f9be2262fe237de6df
Teodor Hristov <justteshi@gmail.com>
2023-02-01 12:47:32 +0200

    Add/ 07/13 update form

--- src/Controller/ArticleAdminController.php
+++ src/Controller/ArticleAdminController.php
@@ -37,13 +37,25 @@ class ArticleAdminController extends AbstractController
     }
 
     /**
-     * @Route ("/admin/article/{id}/edit")
+     * @Route ("/admin/article/{id}/edit", name="admin_article_edit")
      * @IsGranted("MANAGE", subject="article")
      */
-    public function edit(Article $article)
+    public function edit(Article $article, Request $request, EntityManagerInterface $em)
     {
-//        $this->denyAccessUnlessGranted('MANAGE', $article);
-        dd($article);
+        $form = $this->createForm(ArticleFormType::class, $article);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            /** @var Article $article */
+            $article = $form->getData();
+            $em->persist($article);
+            $em->flush();
+
+            $this->addFlash('success', 'Article Updated !');
+            return $this->redirectToRoute('admin_article_list');
+        }
+        return $this->render('article_admin/edit.html.twig', [
+            'articleForm' => $form->createView(),
+        ]);
     }
 
     /**
