10bdd8983783279d4dc0741b29fdadc2cef073c1
Teodor Hristov <justteshi@gmail.com>
2023-02-01 16:11:03 +0200

    Add/ 07/14

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Form\UserRegistrationFormType;
 use App\Security\LoginFormAuthenticator;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -53,13 +54,16 @@ class SecurityController extends AbstractController
         LoginFormAuthenticator $formAuthenticator
     )
     {
-        if ($request->isMethod('POST')) {
-            $user = new User();
-            $user->setEmail($request->request->get('email'));
-            $user->setFirstName('Teo');
+        $form = $this->createForm(UserRegistrationFormType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            /** @var User $user */
+            $user = $form->getData();
+
             $user->setPassword($passwordEncoder->encodePassword(
                 $user,
-                $request->request->get('password')
+                $user->getPassword()
             ));
 
             $em = $this->getDoctrine()->getManager();
@@ -73,6 +77,8 @@ class SecurityController extends AbstractController
                 'main'
             );
         }
-        return $this->render('security/register.html.twig');
+        return $this->render('security/register.html.twig', [
+            'registrationForm' => $form->createView()
+        ]);
     }
 }
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -33,7 +33,7 @@ class User implements UserInterface
     private $roles = [];
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(type="string", length=255, nullable=True)
      * @Groups("main")
      */
     private $firstName;
