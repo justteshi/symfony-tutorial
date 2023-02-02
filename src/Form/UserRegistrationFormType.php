3157ee09ba66918604c6a4a19c03c4664a9b49b6
Teodor Hristov <justteshi@gmail.com>
2023-02-02 18:09:53 +0200

    Add/ 07/17

--- src/Form/UserRegistrationFormType.php
+++ src/Form/UserRegistrationFormType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -28,7 +29,8 @@ class UserRegistrationFormType extends AbstractType
                         'minMessage' => 'Enter longer password'
                     ])
                 ]
-             ]);
+             ])
+            ->add('agreedTerms', CheckboxType::class)
         ;
     }
 
